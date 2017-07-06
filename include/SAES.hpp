ac82d7dfebd13b728fb0c2f1d77e77fd4c49a7c3
Ghabriel Nunes <ghabriel.nunes@gmail.com>
2017-07-06 10:11:39 -0300

    Simplified AES working (temp. mult table)

--- include/SAES.hpp
+++ include/SAES.hpp
@@ -7,14 +7,18 @@
 #include <unordered_map>
 #include "traits.hpp"
 
+#include "debug.hpp"
+#include "xtrace.hpp"
+
 namespace crypto {
     class SAES {
      public:
         using InputType = uint16_t;
         using HalfKey = uint8_t;
-        std::string apply(InputType message, const InputType& key) const;
+        InputType apply(InputType message, const InputType& key) const;
 
         const static std::unordered_map<HalfKey, HalfKey> sbox;
+        const static std::array<std::array<uint8_t, 14>, 14> multTable;
 
      private:
         std::vector<HalfKey> keyExpansion(const InputType& key) const;
@@ -24,11 +28,45 @@ namespace crypto {
         }
 
         HalfKey rotateNibbles(const HalfKey& value) const {
-            return ((value & 0x0F) << 2) | ((value & 0xF0) >> 2);
+            return ((value & 0x0F) << 4) | ((value & 0xF0) >> 4);
         }
 
-        HalfKey substNibbles(const HalfKey& value) const {
-            return (sbox.at(value & 0xF0) << 4) | sbox.at(value & 0x0F);
+        template<typename T>
+        uint8_t nibble(const T& value, size_t index) const {
+            size_t numNibbles = 2 * sizeof(value);
+            auto shift = 4 * (numNibbles - index - 1);
+            auto mask = 0xF << shift;
+            return (value & mask) >> shift;
+        }
+
+        InputType nibbleSwap(const InputType& value) const {
+            InputType result = 0;
+            result |= value & 0xF000;
+            result |= (value & 0x000F) << 8;
+            result |= value & 0x00F0;
+            result |= (value & 0x0F00) >> 8;
+            return result;
+        }
+
+        template<typename T>
+        T substNibbles(const T& value) const {
+            size_t numNibbles = 2 * sizeof(value);
+            T result = 0;
+            for (size_t i = 0; i < numNibbles; i++) {
+                auto shift = 4 * (numNibbles - 1 - i);
+                auto mask = 0xF << shift;
+                result |= sbox.at((value & mask) >> shift) << shift;
+            }
+
+            return result;
+        }
+
+        uint8_t product(uint8_t a, uint8_t b) const {
+            if (a <= 1 || b <= 1) {
+                return a * b;
+            }
+
+            return multTable[a - 2][b - 2];
         }
     };
 }
--- src/SAES.cpp
+++ src/SAES.cpp
@@ -22,28 +22,63 @@ const std::unordered_map<HalfKey, HalfKey> crypto::SAES::sbox = {
     {0xF, 0x7},
 };
 
-std::string crypto::SAES::apply(InputType state, const InputType& key) const {
-    // TODO
-    return "";
+const std::array<std::array<uint8_t, 14>, 14> crypto::SAES::multTable = {{
+    {4, 6, 8, 10, 12, 14, 3, 1, 7, 5, 11, 9, 15, 13},
+    {6, 5, 12, 15, 10, 9, 11, 8, 13, 14, 7, 4, 1, 2},
+    {8, 12, 3, 7, 11, 15, 6, 2, 14, 10, 5, 1, 13, 9},
+    {10, 15, 7, 2, 13, 8, 14, 11, 4, 1, 9, 12, 3, 6},
+    {12, 10, 11, 13, 7, 1, 5, 3, 9, 15, 14, 8, 2, 4},
+    {14, 9, 15, 8, 1, 6, 13, 10, 3, 4, 2, 5, 12, 11},
+    {3, 11, 6, 14, 5, 13, 12, 4, 15, 7, 10, 2, 9, 1},
+    {1, 8, 2, 11, 3, 10, 4, 13, 5, 12, 6, 15, 7, 14},
+    {7, 13, 14, 4, 9, 3, 15, 5, 8, 2, 1, 11, 6, 12},
+    {5, 14, 10, 1, 15, 4, 7, 12, 2, 9, 13, 6, 8, 3},
+    {11, 7, 5, 9, 14, 2, 10, 6, 1, 13, 15, 3, 4, 8},
+    {9, 4, 1, 12, 8, 5, 2, 15, 11, 6, 3, 14, 10, 7},
+    {15, 1, 13, 3, 2, 12, 9, 7, 6, 8, 4, 10, 11, 5},
+    {13, 2, 9, 6, 4, 11, 1, 14, 12, 3, 8, 7, 5, 10}
+}};
+
+InputType crypto::SAES::apply(InputType state, const InputType& key) const {
+    auto w = keyExpansion(key);
+
+    auto key0 = (w[0] << 8) | w[1];
+    auto key1 = (w[2] << 8) | w[3];
+    auto key2 = (w[4] << 8) | w[5];
+
+    state ^= key0;
+
+    InputType round1 = nibbleSwap(substNibbles(state));
+    auto s00 = product(1, nibble(round1, 0)) ^ product(4, nibble(round1, 1));
+    auto s10 = product(4, nibble(round1, 0)) ^ product(1, nibble(round1, 1));
+    auto s01 = product(1, nibble(round1, 2)) ^ product(4, nibble(round1, 3));
+    auto s11 = product(4, nibble(round1, 2)) ^ product(1, nibble(round1, 3));
+    InputType s = 0;
+    s |= s00 << 12;
+    s |= s10 << 8;
+    s |= s01 << 4;
+    s |= s11;
+    round1 = s ^ key1;
+
+    return nibbleSwap(substNibbles(round1)) ^ key2;
 }
 
 std::vector<HalfKey> crypto::SAES::keyExpansion(const InputType& key) const {
     std::vector<HalfKey> result;
-    result.reserve(6);
 
     constexpr static HalfKey rcon1 = 0x80;
     constexpr static HalfKey rcon2 = 0x30;
 
-    HalfKey w0 = (key & 0xFF00) >> 8;
-    HalfKey w1 = key & 0xFF;
-    auto w2 = w0 ^ rcon1 ^ substNibbles(rotateNibbles(w1));
-    auto w3 = w2 ^ w1;
-    auto w4 = w2 ^ rcon2 ^ substNibbles(rotateNibbles(w2));
-    auto w5 = w4 ^ w3;
+    std::array<HalfKey, 6> w;
 
-    result.push_back(w0);
-    result.push_back(w1);
+    w[0] = (key & 0xFF00) >> 8;
+    w[1] = key & 0xFF;
+    w[2] = w[0] ^ rcon1 ^ substNibbles(rotateNibbles(w[1]));
+    w[3] = w[2] ^ w[1];
+    w[4] = w[2] ^ rcon2 ^ substNibbles(rotateNibbles(w[3]));
+    w[5] = w[4] ^ w[3];
 
-    // TODO
+    result.insert(result.end(), w.begin(), w.end());
+
     return result;
 }
